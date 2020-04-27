05b25603207f8bd3018b4503fb788a9f810e04ed
txwin <86094075@qq.com>
2020-04-27 18:18:14 +0800

    FileStorage

---
+++ app/src/main/java/com/example/fragment/FileStorage.java
@@ -0,0 +1,59 @@
+package com.example.fragment;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FileStorage {
+    public boolean WriteFile(Context context, String filename, String data) {
+        try {
+            FileOutputStream fileOutputStream = context.openFileOutput(filename,
+                    Context.MODE_APPEND);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+            writer.write(data);
+            writer.flush();
+            writer.close();
+            fileOutputStream.close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public List<Map<String, Object>> ReadFile(Context context, String filename) {
+        String[] data = new String[2];
+        String message;
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        try {
+            FileInputStream fileInputStream = context.openFileInput(filename);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+            while ((message = reader.readLine()) != null) {
+                data = message.split("&");
+                Map<String, Object> map_data = new HashMap<String, Object>();
+                map_data.put("account", data[0]);
+                map_data.put("password", data[1]);
+                list.add(map_data);
+            }
+            reader.close();
+            fileInputStream.close();
+        } catch (IOException e) {
+            return null;    //文件不存在时或读出失败时返回null
+        }
+
+        return list;
+    }
+}
--- app/src/main/java/com/example/fragment/Register.java
+++ app/src/main/java/com/example/fragment/Register.java
@@ -2,80 +2,147 @@ package com.example.fragment;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Looper;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
     private Button button;
     private Button button_dialog;
-    private EditText editText_style,editText_account,editText_password,editText_confirm;
+    private EditText editText_style, editText_account, editText_password, editText_confirm;
     private String style;
     private Intent intent;
     private Bundle bundle;
+    private FileStorage fileStorage = new FileStorage();
+    private boolean Filestate = false;
+    private boolean registerstate = true;
 
-    private int progress=0;
-    private boolean log[]=new boolean[]{false,false,false,false};
+    private int progress = 0;
+    private boolean log[] = new boolean[]{false, false, false, false};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            for (int x = 0; x < permissions.length; x++) {
+                if (grantResults[x] == PackageManager.PERMISSION_GRANTED) {
+                    //采用文件存储方式保存注册用户信息
+                    if (fileStorage.WriteFile(Register.this, "data.txt",
+                            editText_account.getText().toString() + "&" + editText_password.getText().toString() + "\n")) {
+                        Filestate = true;
+                    } else {
+                        Filestate = false;
+                    }
+                }
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        button=(Button)findViewById(R.id.button3);
-        button_dialog=(Button)findViewById(R.id.button_dialog);
-        editText_style=(EditText)findViewById(R.id.editText6);
-        editText_account=findViewById(R.id.editText3);
-        editText_confirm=findViewById(R.id.editText5);
-        editText_password=findViewById(R.id.editText4);
-        intent=getIntent();
+        button = (Button) findViewById(R.id.button3);
+        button_dialog = (Button) findViewById(R.id.button_dialog);
+        editText_style = (EditText) findViewById(R.id.editText6);
+        editText_account = findViewById(R.id.editText3);
+        editText_confirm = findViewById(R.id.editText5);
+        editText_password = findViewById(R.id.editText4);
+        intent = getIntent();
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!editText_style.getText().toString().equals("")&&!editText_account.getText().toString().equals("")&&!editText_password.getText().toString().equals("")
-                        &&!editText_confirm.getText().toString().equals("")&&editText_password.getText().toString().equals(editText_confirm.getText().toString())){
-                    final ProgressDialog progressDialog=new ProgressDialog(Register.this);
-                    progressDialog.setTitle("QQ会员");
-                    progressDialog.setIcon(R.drawable.vip);
-                    progressDialog.setMessage("正在注册QQ会员，请等待...");
-                    progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-                    progressDialog.show();
-                    new Thread(){
-                        @Override
-                        public void run() {
-                            super.run();
-                            while(progress<100){
-                                progress++;
-                                try {
-                                    sleep(50);
-                                    progressDialog.setProgress(progress);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
+                if (!editText_style.getText().toString().equals("") && !editText_account.getText().toString().equals("") && !editText_password.getText().toString().equals("")
+                        && !editText_confirm.getText().toString().equals("") && editText_password.getText().toString().equals(editText_confirm.getText().toString())) {
+                    fileStorage.ReadFile(Register.this, "data.txt");
+
+                    //判断输入的账号是否已经存在
+                    List<Map<String, Object>> list = fileStorage.ReadFile(Register.this, "data" +
+                            ".txt");
+                    //判断文件是否存在，通过ReadFile()方法的返回值获取
+                    if (list != null) {
+                        for (Map<String, Object> map : list) {
+                            if (map.get("account").equals(editText_account.getText().toString())) {
+                                Toast.makeText(Register.this, "账号已存在", Toast.LENGTH_SHORT).show();
+                                registerstate = false;
+                                break;
+                            } else {
+                                registerstate = true;
                             }
-                            progress=0;     //进度值置位
-                            progressDialog.dismiss();    //关闭进度对话框
-                            //打包注册信息回LoginActivity
-                            bundle=new Bundle();
-                            bundle.putString("account",editText_account.getText().toString());
-                            bundle.putString("password",editText_password.getText().toString());
-                            intent.putExtra("info",bundle);
-                            setResult(1,intent);
-                            finish();
-                            Looper.prepare();   //子线程不会创建Looper，要手动加入消息队列（makeText方法中会创建Handle）
-                            Toast.makeText(getApplicationContext(),"注册成功！", Toast.LENGTH_SHORT).show();
-                            Looper.loop();      //开始工作，之后的代码不会执行
                         }
-                    }.start();
-                }else if(!editText_password.getText().toString().equals(editText_confirm.getText().toString())){
-                    Toast.makeText(getApplicationContext(),"密码不一致", Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(getApplicationContext(),"注册内容不能为空", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                    //不存在重复账号后注册账号
+                    if (registerstate) {
+                        final ProgressDialog progressDialog = new ProgressDialog(Register.this);
+                        progressDialog.setTitle("QQ会员");
+                        progressDialog.setIcon(R.drawable.vip);
+                        progressDialog.setMessage("正在注册QQ会员，请等待...");
+                        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                        progressDialog.show();
+
+                        //动态请求写存储权限
+                        requestPermissions(new String[]{"android.permission" +
+                                ".WRITE_EXTERNAL_STORAGE"}, 1);
+
+                        new Thread() {
+                            @Override
+                            public void run() {
+                                super.run();
+                                while (progress < 100) {
+                                    progress++;
+                                    try {
+                                        sleep(50);
+                                        progressDialog.setProgress(progress);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                progress = 0;     //进度值置位
+                                progressDialog.dismiss();    //关闭进度对话框
+                                //打包注册信息回LoginActivity
+                                bundle = new Bundle();
+                                bundle.putString("account", editText_account.getText().toString());
+                                bundle.putString("password",
+                                        editText_password.getText().toString());
+                                intent.putExtra("info", bundle);
+                                setResult(1, intent);
+                                finish();
+
+                                Looper.prepare();   //子线程不会创建Looper，要手动加入消息队列（makeText方法中会创建Handle）
+                                if (Filestate) {
+                                    Toast.makeText(getApplicationContext(), "注册成功！",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "注册失败！",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                Looper.loop();      //开始工作，之后的代码不会执行
+                            }
+                        }.start();
+                    }
+                } else if (!editText_password.getText().toString().equals(editText_confirm.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "密码不一致", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "注册内容不能为空", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -85,7 +152,9 @@ public class Register extends AppCompatActivity {
             public void onClick(View view) {
                 /*****单选对话框*******/
 //                new AlertDialog.Builder(register.this).setTitle("个性签名")
-//                        .setIcon(getResources().getDrawable(R.drawable.myicon)).setSingleChoiceItems(getResources().getStringArray(R.array.style), -1, new DialogInterface.OnClickListener() {
+//                        .setIcon(getResources().getDrawable(R.drawable.myicon))
+//                        .setSingleChoiceItems(getResources().getStringArray(R.array.style), -1,
+//                        new DialogInterface.OnClickListener() {
 //                    @Override
 //                    public void onClick(DialogInterface dialogInterface, int i) {
 //                        style=getResources().getStringArray(R.array.style)[i];
@@ -98,7 +167,9 @@ public class Register extends AppCompatActivity {
 //                }).setNegativeButton("退出",null).create().show();
                 /*****多选对话框*******/
 //                new AlertDialog.Builder(register.this).setTitle("个性签名")
-//                        .setIcon(R.drawable.myicon).setMultiChoiceItems(getResources().getStringArray(R.array.style), log, new DialogInterface.OnMultiChoiceClickListener() {
+//                        .setIcon(R.drawable.myicon).setMultiChoiceItems(getResources()
+//                        .getStringArray(R.array.style), log, new DialogInterface
+//                        .OnMultiChoiceClickListener() {
 //                    @Override
 //                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
 //                        log[i]=b;
@@ -109,23 +180,25 @@ public class Register extends AppCompatActivity {
 //                        editText.setText("");
 //                        for(int x=0;x<getResources().getStringArray(R.array.style).length;x++){
 //                            if(log[x]){
-//                                editText.append(getResources().getStringArray(R.array.style)[x]+",");
+//                                editText.append(getResources().getStringArray(R.array.style)
+//                                [x]+",");
 //                            }
 //                        }
 //                    }
 //                }).setNegativeButton("退出",null).create().show();
                 /*****自定义对话框*******/
-                Reg_style_Dialog regStyleDialog=new Reg_style_Dialog(Register.this, new Reg_style_Dialog.Reg_style_Dialog_listener() {
-                    @Override
-                    public void getstyle(String[] style) {  //7重写getstyle（）方法
-                        editText_style.setText("");
-                        String Style="";
-                        for(int i=0;i<style.length;i++){
-                            Style+=style[i]+" ";
-                        }
-                        editText_style.setText(Style);
-                    }
-                });
+                Reg_style_Dialog regStyleDialog = new Reg_style_Dialog(Register.this,
+                        new Reg_style_Dialog.Reg_style_Dialog_listener() {
+                            @Override
+                            public void getstyle(String[] style) {  //7重写getstyle（）方法
+                                editText_style.setText("");
+                                String Style = "";
+                                for (int i = 0; i < style.length; i++) {
+                                    Style += style[i] + " ";
+                                }
+                                editText_style.setText(Style);
+                            }
+                        });
                 regStyleDialog.show();
             }
         });
